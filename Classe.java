6e25e2b648b97b8c81a69f97bf096f3b5a66e8ab
Alex Nunes <70280581+alexlmnunes@users.noreply.github.com>
2025-11-10 21:25:36 -0300

    add classe Classe

---
+++ Classe.java
@@ -0,0 +1,10 @@
+public class Classe {
+    private String nome;
+    private String arma;
+    private double forca;
+    private double hp;
+    private double armadura;
+    private String bonus;
+    private String penalidade;
+    private String habilidade_especial;
+}
