192699f7f68bc192696035d038319e20e1d4e3c6
christian <christianrodriguesdc2501@gmail.com>
2025-11-17 22:41:25 -0300

    formatação, correção.

--- src/BarraVida.java
+++ src/BarraVida.java
@@ -1,31 +1,28 @@
 public class BarraVida {
 
-    private static final String RESET  = "\u001B[0m";
-    private static final String GREEN  = "\u001B[32m";
-    private static final String YELLOW = "\u001B[33m";
-    private static final String RED    = "\u001B[31m";
+    private static final String RESET = "\u001B[0m";
+    private static final String VERDE = "\u001B[32m";
+    private static final String AMARELO = "\u001B[33m";
+    private static final String VERMELHO = "\u001B[31m";
 
-    public static void printHealthBar(int current) {
-        int max = 10;       // vida máxima fixa
-        int total = 10;     // largura da barra fixa
-        int filled = current; // 1 HP = 1 bloco
-        double ratio = (double) current / max;
-        String color;
+    public static void mostrarBarra(int vidaAtual) {
 
-        if (ratio <= 0.25)      color = RED;
-        else if (ratio <= 0.60) color = YELLOW;
-        else                    color = GREEN;
+        int vidaMaxima = 10;
+        int blocosCheios = vidaAtual;
+        int blocosVazios = vidaMaxima - vidaAtual;
+        String cor;
 
-        System.out.print(color + "[");
-        for (int i = 0; i < filled; i++) System.out.print("#");
-        for (int i = filled; i < total; i++) System.out.print("-");
-        System.out.println("] " + current + "/" + max + RESET);
+        double porcentagem = (double) vidaAtual / vidaMaxima;
+
+        if (porcentagem <= 0.25) cor = VERMELHO;
+        else if (porcentagem <= 0.60) cor = AMARELO;
+        else cor = VERDE;
+
+        System.out.print(cor + "[");
+        
+        for (int i = 0; i < blocosCheios; i++) System.out.print("#");
+        for (int i = 0; i < blocosVazios; i++) System.out.print(".");
+
+        System.out.println("] " + vidaAtual + "/" + vidaMaxima + RESET);
     }
-
-    //public static void main(String[] args) {
-       // printHealthBar(10);
-       // printHealthBar(7);
-        //printHealthBar(3);
-       // printHealthBar(1);
-    //}
 }
