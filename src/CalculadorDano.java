f0ff7458b4843414d1b098f89deb7ebe99fd6777
lucas20023004 <lucas_massa42@outlook.com>
2025-11-20 19:24:06 -0300

    CalcularDanoqsepronto

--- src/CalculadorDano.java
+++ src/CalculadorDano.java
@@ -38,9 +38,61 @@ if (atacante.getTipoClasse().equals(ConfiguracaoClasse.SECUTOR) && !defensor.get
 danoBase -=1;
 }
 
+//5. APLICA HABILIDADES ESPECIAIS DA ARMA
+aplicarHabilidadeArma(atacante,defensor,arma);
+
+//6. APLICA ARMADURA(SE A ARMA NAO IGNORAR)
+int danoFinal = danoBase;
+if (!arma.ignoraArmadura() && defensor.temArmadura()){
+    danoFinal = danoBase - defensor.getReducaoDano();
+    if (danoFinal < 0) dano final = 0;
+}
+
+return danoFinal;
 
 }
+
+private void  aplicarHabilidadeArma(Gladiador atacante, Gladiador defensor, Arma arma);
+String habilidade = arma.getHabilidadeEspecial();
+
+switch(habilidade){
+    case "Duplo Ataque":
+        if(Aleatorio.chance(arma.getChanceHabilidade())){
+            system.out.println(" DUPLO ATAQUE");
+            // Segundo ataque será executado no próximo turno
+        }
+        break;
+
+    case "Desarmar":
+        if(Aleatorio.chance(arma.getChanceHabilidade())){
+            defensor.getStatus().Desarmar();
+            system.out.println(" DESARME! Oponente perde arma! ")
+        }
+        break;
+
+    case "Quebrar Armadura":
+        defensor.getStatus().adicionarGolpeEscudo();
+        if(defensor.getStatus().getGolpesNoEscudo() >= 2){
+            defensor.quebrarArmadura();
+            system.out.println(" ARMADURA DESTRUÍDA ");
+        }
+        break;
+
+    case "Paralisar":
+        defensor.getStatus().Paralisar();
+        system.out.println(" PARALISADO! Perde o próximo turno")
+        break;    
 }
+    //SANGRAMENTO DO THRAEX
+    if(atacante.getTipoCLasse().equals(ConfiguracaoClasse.THRAEX)){
+       if(Aleatorio.chance(20)){
+        defensor.getStatus().causarSangramento();
+        system.out.println(" SANGRAMENTO! 1 DANO POR 2 TURNOS !")
+       } 
+
+    }
+}
+
 
 
 
